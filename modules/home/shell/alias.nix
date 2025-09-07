5064e0e9ebeae39275fb48dda27a67f8b7fa7f71
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2025-09-07 23:22:48 +0600

    clean: unused variables

--- modules/home/shell/alias.nix
+++ modules/home/shell/alias.nix
@@ -10,15 +10,9 @@
       gc = "git commit -m";
       gp = "git push";
       gpl = "git pull";
-      #dev aliases
-      npm = "pnpm";
-      npx = "pnpm dlx";
     };
     sessionVariables = {
       EDITOR = "nvim";
-      PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
-      PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
-      PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
     };
   };
 }
