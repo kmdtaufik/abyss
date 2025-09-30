48d210da0b0164d55cd4497a843e9202b811f68c
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2025-10-01 00:19:51 +0600

    fix: gesture

--- modules/home/hypr/hyprland.nix
+++ modules/home/hypr/hyprland.nix
@@ -90,8 +90,6 @@
       };
 
       gestures = {
-        workspace_swipe = true;
-        workspace_swipe_fingers = 3;
         workspace_swipe_distance = 500;
         workspace_swipe_invert = true;
         workspace_swipe_min_speed_to_force = 30;
@@ -99,6 +97,9 @@
         workspace_swipe_create_new = true;
         workspace_swipe_forever = true;
       };
+      gesture = [
+        "3, horizontal, workspace"
+      ];
 
       binds = {
         workspace_back_and_forth = true;
