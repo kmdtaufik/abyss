496a48efe5f92eb3870f1162b2ebb763b7cdc806
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2025-10-01 19:51:58 +0600

    hypr: refactor

---
+++ modules/home/hypr/conf/gestures.nix
@@ -0,0 +1,15 @@
+{
+  wayland.windowManager.hyprland.settings = {
+    gestures = {
+      workspace_swipe_distance = 500;
+      workspace_swipe_invert = true;
+      workspace_swipe_min_speed_to_force = 30;
+      workspace_swipe_cancel_ratio = 0.5;
+      workspace_swipe_create_new = true;
+      workspace_swipe_forever = true;
+    };
+    gesture = [
+      "3, horizontal, workspace"
+    ];
+  };
+}
