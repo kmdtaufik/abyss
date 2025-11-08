e27290bfa04a41d094f878f839ed894f7042c0ef
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2025-11-08 21:05:43 +0600

    pkgs: removed nvf config

--- modules/pkgs/nvim/keyamps.nix
+++
@@ -1,3 +0,0 @@
-{
-  vim.globals.mapleader = " ";
-}
--- modules/pkgs/nvim/plugins/mini.nix
+++
@@ -1,9 +0,0 @@
-{
-  vim.mini = {
-    ai.enable = true;
-    pairs.enable = true;
-    align.enable = true;
-    completion.enable = true;
-    icons.enable = true;
-  };
-}
