cc0d81c88922b90288862e76812423afe1db2071
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2025-12-30 17:59:50 +0600

    !chore: fixing updated config

--- modules/home/hypr/conf/autostart.nix
+++ modules/home/hypr/conf/autostart.nix
@@ -1,16 +1,13 @@
 {
   wayland.windowManager.hyprland.settings.exec-once = [
-    # "gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg"
-    # "systemctl --user enable --now hyprpolkitagent.service"
     "hyprctl setcursor Bibata-Modern-Ice 24"
     "swaync"
     "elephant"
     "systemctl --user enable --now hypridle.service"
     "hyprpaper"
     "wl-paste --watch cliphist store"
-    "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-    #"dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-    "xdg-run"
+    "dbus-update-activation-environment --all --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+    # "xdg-run"
     "gtk.sh"
     "launch-bar"
     "restore-wallpaper"
