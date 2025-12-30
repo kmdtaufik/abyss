376924d0957fc755aab529d46c81be3391b0afab
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2025-12-31 00:10:08 +0600

    chore: update neovim

--- modules/home/nvim.nix
+++ modules/home/nvim.nix
@@ -1,6 +1,32 @@
-{
+{pkgs, ...}: {
   programs.neovim = {
     enable = true;
     defaultEditor = true;
+    extraPackages = with pkgs; [
+      gcc
+      nodejs
+      typescript
+      typescript-language-server
+      vscode-langservers-extracted
+      tailwindcss-language-server
+      prettierd
+      cargo
+      rustc
+      rust-analyzer
+      rustfmt
+      clang-tools
+      python3
+      pyright
+      black
+      isort
+      nixd
+      alejandra
+      marksman
+      markdownlint-cli
+      texlab
+      tectonic
+      (lua5_1.withPackages (ps: with ps; [luarocks]))
+      lua-language-server
+    ];
   };
 }
