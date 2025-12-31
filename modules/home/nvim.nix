4d96e363af6eeaf5d5b1920d1f56e7b669101e68
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2025-12-31 22:09:51 +0600

    chore(nvim): fix aliases

--- modules/home/nvim.nix
+++ modules/home/nvim.nix
@@ -2,6 +2,9 @@
   programs.neovim = {
     enable = true;
     defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
     extraPackages = with pkgs; [
       gcc
       nodejs
