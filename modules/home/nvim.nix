a17fd9149659edc76be010ef71447b765f384436
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2026-02-18 18:50:51 +0600

    chore: removed unused pkgs

--- modules/home/nvim.nix
+++ modules/home/nvim.nix
@@ -5,32 +5,18 @@
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
+    withPython3 = true;
+    withNodeJs = true;
     extraPackages = with pkgs; [
       gcc
-      nodejs
-      typescript
-      typescript-language-server
-      vscode-langservers-extracted
-      tailwindcss-language-server
-      prettierd
       cargo
       rustc
       rust-analyzer
-      rustfmt
-      clang-tools
-      python3
-      pyright
-      black
-      isort
       nixd
       alejandra
-      marksman
-      markdownlint-cli
-      texlab
-      tectonic
-      (lua5_1.withPackages (ps: with ps; [luarocks]))
-      lua-language-server
       tree-sitter
+      python3
+      nodejs_24
     ];
   };
 }
