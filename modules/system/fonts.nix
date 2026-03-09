bb198831335a12a72a4dd252520188a877f9d6d4
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2026-03-09 20:41:41 +0600

    fonts: add google sans flex

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -1,4 +1,30 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  google-sans-flex = pkgs.stdenvNoCC.mkDerivation {
+    pname = "google-sans-flex";
+    version = "1.0";
+
+    src = ./google-sans-flex;
+
+    dontBuild = true;
+
+    installPhase = ''
+      runHook preInstall
+
+      # Create the standard font directory
+      mkdir -p $out/share/fonts/truetype
+
+      cp *.ttf $out/share/fonts/truetype/
+
+      runHook postInstall
+    '';
+
+    meta = with pkgs.lib; {
+      description = "Google Sans Flex ";
+      license = licenses.ofl;
+      platforms = platforms.all;
+    };
+  };
+in {
   fonts.packages = with pkgs; [
     lohit-fonts.bengali
     noto-fonts
@@ -8,10 +34,6 @@
     nerd-fonts.fira-mono
     fira-sans
     nerd-fonts.jetbrains-mono
-    # (google-fonts.override {
-    #   fonts = [
-    #     "Google Sans Flex"
-    #   ];
-    # })
+    google-sans-flex
   ];
 }
