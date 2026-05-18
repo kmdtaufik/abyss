3f1d58774097eaca52f1775e61eec7d8c6505f49
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2026-05-18 21:52:00 +0600

    services: remove ssh-agent

--- modules/home/dev/ssh.nix
+++ modules/home/dev/ssh.nix
@@ -4,9 +4,6 @@
   ...
 }: {
   config = lib.mkIf config.modules.ssh.enable {
-    services.ssh-agent = {
-      enable = true;
-    };
     programs.ssh = {
       enable = true;
       addKeysToAgent = "yes";
