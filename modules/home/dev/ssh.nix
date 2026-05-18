41de630f2e86fc149301d10c9145f5174f87e597
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2026-05-18 20:40:20 +0600

    services: add ssh-agent

--- modules/home/dev/ssh.nix
+++ modules/home/dev/ssh.nix
@@ -4,6 +4,9 @@
   ...
 }: {
   config = lib.mkIf config.modules.ssh.enable {
+    services.ssh-agent = {
+      enable = true;
+    };
     programs.ssh = {
       enable = true;
       addKeysToAgent = "yes";
