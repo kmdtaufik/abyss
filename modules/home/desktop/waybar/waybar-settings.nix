f5ce22b71e397f1772e58fc783943ba6e4a2dc2e
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2026-08-22 20:58:50 +0600

    chore(waybar): fix caffine icon

--- modules/home/desktop/waybar/waybar-settings.nix
+++ modules/home/desktop/waybar/waybar-settings.nix
@@ -233,8 +233,8 @@
         "idle_inhibitor" = {
           format = "{icon}";
           format-icons = {
-            activated = "";
-            deactivated = "";
+            activated = "";
+            deactivated = "";
           };
           tooltip = true;
           tooltip-format-activated = "Coffee Mode Active";
